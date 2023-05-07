8423c86c4dcfd021cd987ae1e99ba02b14b45a0f
Leonel Gauna <leonel.mariano.g@gmail.com>
2023-05-06 22:14:35 -0300

    Add tooling for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 export default function App() {
 	return (
 		<div className="flex items-center justify-center h-screen">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			{/* data-test? https://docs.cypress.io/guides/references/best-practices#Selecting-Elements */}
+			<h1 className="text-3xl font-bold underline" data-test="header-msg">
+				Hello world!
+			</h1>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
+// https://vitest.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		istanbul({
+			cypress: true,
+			requireEnv: false,
+		}),
+	],
+	server: {
+		host: true,
+		port: process.env.PORT ? +process.env.PORT : 5173,
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test.setup.ts',
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['text', 'html', 'clover', 'json', 'lcov'],
+			reportsDirectory: './coverage/vitest',
+		},
+	},
 });
