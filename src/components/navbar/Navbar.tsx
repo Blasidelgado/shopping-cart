ddb7f82aa531e1e234046c9049968a010fcdcfc4
Blasidelgado <blasidelgado@gmail.com>
2024-03-18 12:25:22 -0300

    Add checkout page button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -78,6 +78,18 @@ const Navbar = () => {
 								Shop
 							</Link>
 						</li>
+						<li>
+							<Link
+								to="/checkout"
+								className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${
+									location.pathname === '/checkout'
+										? 'md:text-blue-700 dark:text-white md:dark:text-blue-500'
+										: ''
+								}`}
+							>
+								Checkout
+							</Link>
+						</li>
 					</ul>
 				</div>
 			</div>
