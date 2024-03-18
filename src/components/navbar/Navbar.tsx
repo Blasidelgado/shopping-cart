439951743e9f510aa94fb515469c79a8c9c36c04
Blasidelgado <blasidelgado@gmail.com>
2024-03-18 12:54:56 -0300

    Add navbar cart product count

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ cartCount }) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const location = useLocation();
 
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 	return (
 		<nav className="bg-white border-gray-200 dark:bg-gray-900">
-			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4 mx-5 xl:mx-auto">
 				<Link
 					to="/"
 					className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -28,7 +28,7 @@ const Navbar = () => {
 				<button
 					onClick={toggleNavbar}
 					type="button"
-					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline- focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
 					aria-controls="navbar-default"
 					aria-expanded={isOpen ? 'true' : 'false'}
 				>
@@ -89,6 +89,17 @@ const Navbar = () => {
 							>
 								Checkout
 							</Link>
+						</li>
+						<li className="flex gap-2 py-2 px-3 md:p-0">
+							<span className="text-white">{cartCount}</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="white"
+								className="w-6 h-6"
+							>
+								<path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+							</svg>
 						</li>
 					</ul>
 				</div>
